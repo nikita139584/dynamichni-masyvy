6e65b4c8093e06c8ffd0b2ee8538377589e4f1e1
nikita139584 <staskonikita139@gmail.com>
2025-06-21 10:49:56 +0200

    Create myarray.h

---
+++ myarray.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>  
+using namespace std;
+//void a() {
+//    int size = 4;
+//    int* arr = new int[size + 1];
+//}
+//void b(){
+//    int size = 4;
+//    int* arr = new int[size];
+//    for (int i = 0; i < size; ++i) {
+//        arr[i] = rand() % 100 + 1;
+//    }
+//}
+// void c(){
+//     int size = 4;
+//int* arr = new int[size]; 
+//for (int i = 0; i < size; ++i) {
+//    arr[i] = rand() % 100 + 1;
+//}
+//cout << "array: ";
+//for (int i = 0; i < size; ++i) {
+//    cout << arr[i] << " ";
+//}
+// }
+//void d(int* arr, int size) {
+    //int size = 4;
+    //int* arr = new int[size+1];
+//    delete[] arr; 
+//}
+// void e(){
+   //srand(time(0));
+    //int size = 4;
+    //int* arr = new int[size+1];
+    //for (int i = 0; i < size; ++i) {
+    //    arr[i] = rand() % 100 + 1;
+    //}
+    //cout << "Array: ";
+    //for (int i = 0; i < size; ++i) {
+    //    cout << arr[i] << " ";
+    //}
+    //int newElement = rand() % 100 + 1;
+    //arr[size] = newElement;
+    //cout << "Array: "<< newElement<<" ";
+    //for (int i = 0; i < size; ++i) {
+    //    cout << arr[i] << " ";
+    //}
+    //delete[] arr;
+// }
+
+//void f(){
+//srand(time(0));
+//int size = 4;
+//int* arr = new int[size];
+//for (int i = 0; i < size; ++i) {
+//    arr[i] = rand() % 100 + 1;
+//}
+//cout << "Array: ";
+//for (int i = 0; i < size; ++i) {
+//    cout << arr[i] << " ";
+//}
+//cout << endl;
+//int newElement = rand() % 100 + 1;
+//int* new_arr = new int[size + 1];
+//for (int i = 0; i < size; ++i) {
+//    new_arr[i] = arr[i];
+//}
+//new_arr[size] = newElement;
+//++size;
+//cout << "New array with added element: ";
+//for (int i = 0; i < size; ++i) {
+//    cout << new_arr[i] << " ";
+//}
+//cout << endl;
+//delete[] arr;
+//delete[] new_arr; 
+//}
+//void i{
+    //srand(time(0));
+    //int size = 4;
+    //int* arr = new int[size];
+    //for (int i = 0; i < size; ++i) {
+    //    arr[i] = rand() % 100 + 1;
+    //}
+    //cout << "Array: ";
+    //for (int i = 0; i < size; ++i) {
+    //    cout << arr[i] << " ";
+    //}
+    //int newElement = rand() % 100 + 1;
+    //int* new_arr = new int[size + 1];
+    //for (int i = 0; i < size; ++i) {
+    //    new_arr[i] = arr[i];
+    //}
+    //new_arr[size] = newElement;
+    //--size;
+    //cout << "Array: ";
+    //for (int i = 0; i < size; ++i) {
+    //    cout << new_arr[i] << " ";
+    //}
+    //delete[] arr;
+    //delete[] new_arr;
+//}
+//int main() {
+
+//}
